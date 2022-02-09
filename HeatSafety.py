a66613049ebeaf3512a058722a7005f798142103
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-02-09 10:54:37 -0500

    HeatSafety now can actually read the cam.temp()
    Currently relies on Trigger.py from Teledyne FLIR examples. Does not completely work as the trigger is supposed to. Will work on getting the camera to auto trigger at a specified temperature.

--- HeatSafety.py
+++ HeatSafety.py
@@ -1,54 +1,122 @@
 import PySpin
+import sys
+import os
+import Trigger #This doesn't quite work the way I need it to, it is a stand in for trigger code that will be added
 
-# PseudoCode
-
-SafeTemperature = 40  # Must be reached after each test
-CameraTemp = PySpin.Camera.DeviceTemperature
-
-
-def RunningScript(temperature):
-    while PySpin.Camera.DeviceTemperature < temperature:
-        Heat(temperature)
-
-    PySpin.Camera.Init()
-    PySpin.Camera.DeviceTemperatureSelector
-
-    if PySpin.Camera.DeviceTemperature == temperature:
-        Capture(temperature)
+cameraTemperature = 0
+NUM_IMAGES = 10  # number of images to grab
 
 
-def CoolToSafety():
-    while PySpin.Camera.DeviceTemperature > 50:
-        print("Cooling the Camera to", SafeTemperature, "°C")
-        # Fan().Blow()
-        # print("Heat gun off")
+def run_single_camera(cam):
+    """
+    This function acts as the body of the example; please see NodeMapInfo example
+    for more in-depth comments on setting up cameras.
+
+    :param cam: Camera to run on.
+    :type cam: CameraPtr
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+        err = False
+
+        # Retrieve TL device nodemap and print device information
+        nodemap_tldevice = cam.GetTLDeviceNodeMap()
+
+        result &= print_device_info(nodemap_tldevice)
+
+        # Initialize camera
+        cam.Init()
+
+        # Retrieve GenICam nodemap
+        nodemap = cam.GetNodeMap()
+
+        # Configure trigger
+        if configure_trigger(cam) is False:
+            return False
+
+        # Acquire images
+        result &= acquire_images(cam, nodemap, nodemap_tldevice)
+
+        # Reset trigger
+        result &= reset_trigger(nodemap)
+
+        # Deinitialize camera
+        cam.DeInit()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
 
 
-def Capture(temperature):
-    print("Capturing 10 x images at: ", temperature, "°C")
+# Grabs the temperature of the camera, returns it as a float
+def GetCameraTemperature(cam):
+    x = 0
+    if cam.DeviceTemperature.GetAccessMode() == PySpin.RO:
+        x = cam.DeviceTemperature.ToString()
+
+    x = float(x)
+    return x
 
 
-def Heat(temperature):
-    print("Heating to", temperature, end="\n")
+def Cool(cam):
+    print('Please cool the camera, it is currently: %s' % GetCameraTemperature(cam))
 
-    # While(Temperature < Goal Temp)
-    # Fans().Off
-    # Print("Heat gun on low")
+
+def HeatSafety(cam, GoalTemperature):
+    Temp = GetCameraTemperature(cam)
+    print(GoalTemperature)
+    nodeMap = cam.GetNodeMap()
+    while Temp < GoalTemperature:
+        Temp = GetCameraTemperature(cam)
+        print("Heating")
+
+    if Temp > GoalTemperature:
+        print("Capturing, please stop heating")
+        Trigger.run_single_camera(cam)  # Cites : FLIR TELEDYNE
+    Cool(cam)
 
 
 def main():
-    # Temp Array = ArrayofTemperature() (from 50 -> 80 C)
-    TempArray = []  # Fill the array of temperatures to test the camera on.
+    system = PySpin.System.GetInstance()
 
-    for i in range(50, 85, 5):
-        TempArray.append(i)
+    # Retrieve list of cameras from the system
+    cam_list = system.GetCameras()
 
-    # Run the Running Script
-    for i in TempArray:
-        RunningScript(i)
-        CoolToSafety()
-        print("\n")
+    num_cameras = cam_list.GetSize()
+
+    # From Spinnaker SDK Examples
+    if num_cameras == 0:
+        # Clear camera list before releasing system
+        cam_list.Clear()
+
+        # Release system instance
+        system.ReleaseInstance()
+
+        print('Not enough cameras!')
+        input('Done! Press Enter to exit...')
+        return False
+
+    # gets the camera temperature.
+    # for x in range(40, 85, 5):
+    for i, cam in enumerate(cam_list):
+        cam.Init()
+        HeatSafety(cam, 34)
+
+    del cam
+
+    # Clear camera list before releasing system, this makes a mess if not cleared
+    cam_list.Clear()
+
+    # Release system instance
+    system.ReleaseInstance()
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    if main():
+        sys.exit(0)
+    else:
+        sys.exit(1)
