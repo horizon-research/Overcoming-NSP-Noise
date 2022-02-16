50239bc27e4a1fb0eb5ac56ca22f850823cb7bff
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-02-16 18:51:15 -0500

    This will be the Neural Network Script
    Research required.

---
+++ IgnoresDarkCurrent.py
@@ -0,0 +1,8 @@
+# This will be the Neural Network Script
+
+# TODO
+#  input - Images from camera
+#  Convolution  - to-Learn
+#  ReLU - to-Learn
+#  Convolution - to-Learn
+#  Pooling (this is critical for seeing through the noise in these images)
