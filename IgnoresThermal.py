4893432a01e8059fba3305cd1cddc45babcdb1d3
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-02-21 12:27:39 -0500

    Today: reading in images from the directory

--- IgnoresThermal.py
+++ IgnoresThermal.py
@@ -1,23 +1,11 @@
 # This will be the Neural Network Script
-# Learning NN in real time.
+# Honestly, the most important part is deciding what in the world you are imaging, without that it's hard to figure out
+# what the requirements are. Otherwise, how in the world can you tailor the needs of the NN.
 
 # TODO LIST
 #  input - Images from camera : Import a folder of those images.
-#  Convolution  - to-Learn : Create a Kernel to go over each image
-#  ReLU - to-Learn : "Rectifier"
-#  Source : https://machinelearningmastery.com/rectified-linear-activation-function-for-deep-learning-neural-networks/
-#  Convolution - to-Learn
-#  Pooling (this is critical for seeing through the noise in these images) Also known as Max Pooling.
+#  Design a NN that makes use of pooling.
 
+# TODO today
+# import working file scanner that reads in each image file.
 
-
-# def ImportImages():
-    #Import from folder of sample images.
-
-# def Kernel():
-    #Create a kernel for the network to iterate over, this will help with recognition.
-
-# def Connect():
-    #Connects layers of the neural network
-
-#
