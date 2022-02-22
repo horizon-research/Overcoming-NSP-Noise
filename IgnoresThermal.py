acbd511084714a0750efcc1960e9a23a547170a3
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-02-22 10:34:24 -0500

    Citation:
    Team, K. (n.d.). Keras documentation: Image Classification From Scratch. Keras. Retrieved February 22, 2022, from https://keras.io/examples/vision/image_classification_from_scratch/

--- IgnoresThermal.py
+++ IgnoresThermal.py
@@ -1,7 +1,9 @@
 # This will be the Neural Network Script
-import pathlib
+# Cites : https://keras.io/examples/vision/image_classification_from_scratch/
+# Depends on : pydot and Graphviz
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 
 images = [] # DataSet of Images
 
@@ -26,9 +28,88 @@ Validation = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size,
 )
 
-def main() :
-    print("Success")
+# This is a method useful for our use-case where I doubt I can
+# capture a data set of 10,000.
+data_augmentation = keras.Sequential(
+    [ layers.RandomFlip("horizontal"),
+      layers.RandomRotation(0.1)
+    ]
+)
 
+# For CPU training
+augmented_train_ds = DataSet.map(
+  lambda x, y: (data_augmentation(x, training=True), y)
+)
+
+# Prevents I/O issues
+train_ds = DataSet.prefetch(buffer_size=32)
+val_ds = Validation.prefetch(buffer_size=32)
+
+# Directly Cites : https://keras.io/examples/vision/image_classification_from_scratch/
+def NModel(input_shape,num_classes):
+    inputs = keras.Input(shape = input_shape)
+    x = data_augmentation(inputs)
+    x = layers.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32,3, strides = 2, padding= "same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64,3,padding="Same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x
+
+   
+    for size in [128,256,512,728]:
+       x = layers.Activation("relu")(x)
+       x = layers.SeparableConv2D(size,3,padding = "same")(x)
+       x = layers.BatchNormalization()(x)
+
+       x = layers.Activation("relu")(x)
+       x = layers.SeparableConv2D(size,3,padding = "same")(x)
+       x = layers.BatchNormalization()(x)
+
+       x = layers.MaxPooling2D(3,strides = 2, padding ="same")(x)
+
+       #Residual? TODO : Look up for comprehension
+       residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+       previous_block_activation
+       )
+       x = layers.add([x, residual])  
+       previous_block_activation = x 
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+model = NModel(input_shape=image_size + (3,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True)   
+   
+epochs = 50
+callbacks = [keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+
+model.compile(
+    optimizer = keras.optimizers.Adam(1e-3),
+    loss = "binary_crossentropy",
+    metrics = ["accuracy"],
+)
+
+model.fit(DataSet,epochs = epochs, callbacks = callbacks, validation_data = Validation)
+
+def main() :
+    print("Nothing broke before here, this is exciting")
 
 
 
