ca0a11636322f61ee040e16316b74e32b1ffedeb
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-02-21 22:59:42 -0500

    Add files via upload

--- IgnoresThermal.py
+++ IgnoresThermal.py
@@ -1,11 +1,51 @@
 # This will be the Neural Network Script
-# Honestly, the most important part is deciding what in the world you are imaging, without that it's hard to figure out
-# what the requirements are. Otherwise, how in the world can you tailor the needs of the NN.
+# Honestly, the most important part is deciding what in the world you are imaging.
+# Otherwise, how in the world can you tailor the needs of the NN.
 
 # TODO LIST
 #  input - Images from camera : Import a folder of those images.
-#  Design a NN that makes use of pooling.
+#  Design a CNN that makes use of max-pooling.
 
-# TODO today
+# TODO : today
 # import working file scanner that reads in each image file.
+
+# What could be interesting?
+    # Is a starbucks drink mine? Looking for my name on cups of Starbucks coffee.
+    # Noise will be inducedf
+import pathlib
+
+
+def main() :
+    print("\n")
+
+    for path in pathlib.Path("Training_Data/Refresh").iterdir():
+        if path.is_file():
+            current_file = open(path, "r")
+            print(current_file.name)
+            current_file.close()
+
+    print("\n")
+
+    for path in pathlib.Path("Training_Data/BOSE").iterdir():
+        if path.is_file():
+            current_file = open(path, "r")
+            print(current_file.name)
+            current_file.close()
+
+    print("\n")
+
+    for path in pathlib.Path("Training_Data/WhiteCup").iterdir():
+        if path.is_file():
+            current_file = open(path, "r")
+            print(current_file.name)
+            current_file.close()
+
+    print("\n")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
