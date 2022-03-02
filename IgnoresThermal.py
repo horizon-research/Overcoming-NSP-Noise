37f62022f5347b3e24fe547ae1ff38ed9e1e9c98
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-03-01 22:42:09 -0500

    Add files via upload

--- IgnoresThermal.py
+++ IgnoresThermal.py
@@ -3,14 +3,17 @@
 # Cites : https://www.tensorflow.org/tutorials/images/cnn
 # Depends on : pydot and Graphviz
 import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import ResNet50
+from matplotlib import pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 
+# Reduced batch size to lower the CPU load and to accelerate the processing
+image_size = (180, 180)
+batch_size = 5
 
-
-image_size = (100, 100) #More power needed, but more accuracy gained.
-batch_size = 32
-
+# Creates the DataSet for training from the hot and iced coffee images
 DataSet = tf.keras.preprocessing.image_dataset_from_directory(
     "Training_Data",
     validation_split=0.2,
@@ -20,9 +23,10 @@ DataSet = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size,
 )
 
+# Creates the DataSet for training from the iced and hot coffee images
 Validation = tf.keras.preprocessing.image_dataset_from_directory(
     "Training_Data",
-    validation_split=0.1,
+    validation_split=0.2,
     subset="validation",
     seed=1337,
     image_size=image_size,
@@ -30,20 +34,12 @@ Validation = tf.keras.preprocessing.image_dataset_from_directory(
 )
 
 # This is a method useful for our use-case where I doubt I can
-# capture a data set of 10,000.
+# capture a data set of 10,000, but 464 will do for now.
+# This does not modify the pixels but merely stretches them and
 data_augmentation = keras.Sequential(
-    [layers.RandomFlip("horizontal"),
-     layers.RandomRotation(0.1),
-     layers.RandomFlip("vertical"),
-     ]
+    [layers.RandomFlip("horizontal"), layers.RandomRotation(0.1),
+     layers.RandomFlip("vertical")]
 )
-
-
-# # For CPU training
-augmented_train_ds = DataSet.map(
-    lambda x, y: (data_augmentation(x, training=True), y)
-)
-
 
 # Prevents I/O issues
 train_ds = DataSet.prefetch(buffer_size=32)
@@ -53,113 +49,107 @@ val_ds = Validation.prefetch(buffer_size=32)
 # Directly Cites : https://keras.io/examples/vision/image_classification_from_scratch/
 def NModel(input_shape, num_classes):
     inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
     x = data_augmentation(inputs)
     x = layers.Rescaling(1.0 / 255)(x)
-    x = layers.SeparableConv2D(128, 3, padding="same")(x)
 
-    previous_block_activation = x
-
-    for size in [128, 256, 512, 1024]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)  # Key for noisy images, use more than once?
-
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])
-        previous_block_activation = x
-
-        x = layers.Activation("tanh")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)  # Key for noisy images, use more than once?
-
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])
-
-        previous_block_activation = x
-
-        x = layers.Activation("tanh")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)  # Key for noisy images, use more than once?
-
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])
-        previous_block_activation = x
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)  # Key for noisy images, use more than once?
-
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])
-        previous_block_activation = x
-
-
-
-    # TODO : Look up for comprehension
-    x = layers.SeparableConv2D(128, 3, padding="same")(x)
+    # Entry block
+    x = layers.Conv2D(1, 1, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = layers.Dense(1, activation="softmax")(x)
+    x = layers.Dropout(0.1)(x)
+
+    x = layers.Conv2D(2, 1, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dense(1, activation="softmax")(x)
+    x = layers.Dropout(0.2)(x)
+
+    x = layers.Conv2D(4, 1, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dense(1, activation="softmax")(x)
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Conv2D(16, 1, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dense(1, activation="softmax")(x)
+    x = layers.Dropout(0.4)(x)
 
     x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(1, activation="softmax")(x)
     if num_classes == 2:
         activation = "sigmoid"
         units = 1
     else:
-        activation = "sigmoid"
+        activation = "softmax"
         units = num_classes
 
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(0.1)(x)
     outputs = layers.Dense(units, activation=activation)(x)
     return keras.Model(inputs, outputs)
 
 
-model = NModel(input_shape=image_size + (3,), num_classes=2)
-
-def Compile() :
+def Compile():
+    model = NModel(input_shape=image_size + (3,), num_classes=2)
     keras.utils.plot_model(model, show_shapes=True)
-    epochs = 10 # over-fitting?
+    epochs = 100  # over-fitting?
     callbacks = [keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"), ]
     model.compile(
-        optimizer=keras.optimizers.SGD(1e-3),
+        optimizer=keras.optimizers.Adam(1e-2),
         loss="binary_crossentropy",
-        metrics=["accuracy"],
+        metrics=["accuracy"]
     )
-    model.fit(DataSet, epochs=epochs, callbacks=callbacks, validation_data=Validation)
+
+    model.fit(DataSet, shuffle=True,
+              epochs=epochs, callbacks=callbacks, validation_data=Validation
+              )
+
 
 def Test(image):
+    model = NModel(input_shape=image_size + (3,), num_classes=2)
     img = keras.preprocessing.image.load_img(
         image, target_size=image_size
-        # Test image is of a coffee cup with my name on it from fall of 2020.
     )
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create batch axis
     predictions = model.predict(img_array)
     score = predictions[0]
-    print("This image is %.2f percent your coffee." % (100 * (score)))
+
+    print("This image is %.2f percent hot coffee and this image is %.2f percent iced coffee." % (
+        100 * score, 100 * (1 - score)))
+    if 100 * score > (100 * (1 - score)):
+        print("This image is hot coffee\n")
+    elif 100 * score < (100 * (1 - score)):
+        print("This image is cold coffee\n")
+    else:
+        print("This image is niether hot nor cold coffee")
 
 
 def main():
-    x = input("Please Enter 1 for compile 2 for test and 3 for both\n")
-    if(x == "1"):
+    x = input("Please Enter 1 for compile 2 for test and 3 for both or exit\n")
+
+    if x == "1":
+        print("You have selected compile")
         Compile()
-    elif(x == "2"):
-        print("Image of my coffee")
-        Test("Training_Data/sample-18255214-481-28.png")
-    elif(x == "3"):
+
+    elif x == "2":
+        print("You have selected test")
+        print("Images of hot coffee")
+        Test("HOT.JPG")  # This image is incredibly interesting because it has cold features but is hot.
+        Test("HOT2.jpeg")
+        print("Images of iced coffee")
+        Test("ICED.jpeg")
+        Test("ICED3.jpg")
+
+    elif x == "3":
+        print("You have selected to compile and test")
         Compile()
-        Test("Training_Data/Test.png")
+        print("Images of hot coffee")
+        Test("HOT.JPG")
+        Test("HOT2.jpeg")
+        print("Images of iced coffee")
+        Test("ICED.jpeg")
+        Test("ICED3.jpg")
+
 
 if __name__ == '__main__':
     main()
