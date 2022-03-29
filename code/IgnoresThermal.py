fcb504f1ff033daafeb5072a0222fd99a056e2f6
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-03-29 19:08:01 -0400

    ResNet

--- code/IgnoresThermal.py
+++ code/IgnoresThermal.py
@@ -10,11 +10,11 @@ from tensorflow.keras import layers
 
 # Reduced batch size to lower the CPU load and to accelerate the processing
 image_size = (200, 200)
-batch_size = 5 
+batch_size = 5
 
 # Creates the DataSet for training from the hot and iced coffee images
 DataSet = tf.keras.preprocessing.image_dataset_from_directory(
-    "Training_Data",
+    "CleanTestImages",
     validation_split=0.2,
     subset="training",
     seed=1337,
@@ -23,14 +23,13 @@ DataSet = tf.keras.preprocessing.image_dataset_from_directory(
 )
 # Creates the DataSet for training from the iced and hot coffee images
 Validation = tf.keras.preprocessing.image_dataset_from_directory(
-    "Training_Data",
+    "CleanTestImages",
     validation_split=0.2,
     subset="validation",
     seed=1337,
     image_size=image_size,
     batch_size=batch_size,
 )
-
 
 # This is a method useful for our use-case where I doubt I can
 # capture a data set of 10,000, but 464 will do for now.
@@ -42,8 +41,8 @@ data_augmentation = keras.Sequential(
 )
 
 # Prevents I/O issues
-train_ds = DataSet.prefetch(buffer_size=32)
-val_ds = Validation.prefetch(buffer_size=32)
+train_ds = DataSet.prefetch(buffer_size=5)
+val_ds = Validation.prefetch(buffer_size=5)
 
 
 # Cites this Model as a sample : https://keras.io/examples/vision/image_classification_from_scratch/
@@ -55,83 +54,149 @@ def NModel(input_shape, num_classes):
 
     # Entry block
     """
-    This is a combination between ResNet and XCeption
+    This is a combination between the ResNet and XCeption models. 
     """
-
-    # x = layers.Rescaling(1.0 / 255)(x)
-    x = layers.Activation("relu")(x)
+    x = layers.Rescaling(1.0 / 255)(x)
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(64, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    previous_block_activation = x 
+    residual = layers.Conv2D(64, 3, strides=2, padding="same")(
+        previous_block_activation
+    )
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(64, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = layers.add([x, residual])  # Add back residual
+    previous_block_activation = x 
+    residual = layers.Conv2D(64, 3, strides=2, padding="same")(
+        previous_block_activation
+    )
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(64, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = layers.add([x, residual])  # Add back residual
+    previous_block_activation = x  
+    residual = layers.Conv2D(64, 3, strides=2, padding="same")(
+        previous_block_activation
+    ) 
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(64, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = layers.add([x, residual])  # Add back residual
+    previous_block_activation = x   
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(64, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(64, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
     
-    x = layers.Activation("relu")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x) 
-    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
 
-    x = layers.Activation("relu")(x)
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x) 
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x) 
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x) 
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x) 
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x) 
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x) 
+
+    x = layers.Activation("sigmoid")(x) 
     x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x) 
 
-    previous_block_activation = x  # Set aside residual
+    x = layers.Activation("sigmoid")(x) 
+    x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
 
-    for size in [128, 256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+    x = layers.Activation("sigmoid")(x) 
+    x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
 
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+    x = layers.Activation("sigmoid")(x) 
+    x = layers.Conv2D(256, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x) 
 
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+    x = layers.Activation("sigmoid")(x) 
+    x = layers.Conv2D(256, 3, strides=2, padding="same")(x) 
+    x = layers.BatchNormalization()(x) 
 
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-   
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(512, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(512, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(512, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(512, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(512, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Activation("sigmoid")(x)
+    x = layers.Conv2D(512, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)  
+
+    x = layers.Dropout(0.2)(x)
     x = layers.GlobalAveragePooling2D()(x)
 
     if num_classes == 2:
@@ -148,8 +213,8 @@ def NModel(input_shape, num_classes):
 def Compile():
     model = NModel(input_shape=image_size + (3,), num_classes=2)
     keras.utils.plot_model(model, show_shapes=True)
-    epochs = 200  # over-fitting?
-    callbacks = [keras.callbacks.ModelCheckpoint("IgnoresThermal_{epoch}.h5"), ]
+    epochs = 1  # over-fitting?
+    callbacks = [keras.callbacks.ModelCheckpoint("NoThermal_at_{epoch}.h5"), ]
     model.compile(
         optimizer=keras.optimizers.Adam(0.01),
         loss="binary_crossentropy",
@@ -159,7 +224,6 @@ def Compile():
     model.fit(DataSet, shuffle=True,
               epochs=epochs, callbacks=callbacks, validation_data=Validation
               )
-
 
 def Test(image):
     model = NModel(input_shape=image_size + (3,), num_classes=2)
@@ -186,7 +250,7 @@ def Test(image):
 
 
 def Statistics(img1, img2, img3, img4):
-    Accuracy = open("Accuracy.json", "r")
+    Accuracy = open("CleanAccuracy.json", "r")
     load = json.load(Accuracy)
     Acc = load['Accuracy'] * load['Trials']
     print("Current accuracy is %f: " % load['Accuracy'])
@@ -209,7 +273,7 @@ def Statistics(img1, img2, img3, img4):
     if not T4:
         A += 1
     Acc = (A + Acc) / Nt
-    Accuracy = open("Accuracy.json", "w")
+    Accuracy = open("CleanAccuracy.json", "w")
     load["Accuracy"] = Acc
     load["Trials"] = load["Trials"] + A
     print("Current final accuracy is %f: " % Acc)
@@ -236,13 +300,16 @@ def main():
         Statistics('HOT2.jpeg', 'HOT3.jpg', 'ICED4.jpg', "ICED5x.jpg")
     
     elif x == "c": #  Current Accuracy
-        Accuracy = open("Accuracy.json", "r")
+        Accuracy = open("CleanAccuracy.json", "r")
         load = json.load(Accuracy)
         Accuracy.close()
         Acc = load['Accuracy'] * 100
         Acc = round(Acc,3)
         out = ("The current accuracy {Acc} %").format(Acc = Acc)
         print(out)
+        
+        
+
 
 if __name__ == '__main__':
     main()
